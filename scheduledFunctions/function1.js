0ef8aac2b0f79c8cb6c72df0e420fc283443364e
DeadBoyPiotrek <87661735+DeadBoyPiotrek@users.noreply.github.com>
2022-10-03 15:12:41 +0200

    no puppeteer

--- scheduledFunctions/function1.js
+++ scheduledFunctions/function1.js
@@ -3,9 +3,9 @@ import { getPrice } from '../index.js';
 import { uploadScreenshots } from '../mongoDB/mongodb2.js';
 const initScheduledJobs = () => {
   const scheduledJobFunction = CronJob.schedule('*/1 * * * *', () => {
-    getPrice();
+    // getPrice();
     uploadScreenshots({ image: 'some image but automated' });
-    console.log("I'm executed on a schedule!");
+    // console.log("I'm executed on a schedule!");
   });
 
   scheduledJobFunction.start();
