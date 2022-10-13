139d551509d66df908b190e4b689f60d03f82b03
Piotr Turski <pipolito2115@gmial.com>
2022-10-13 17:42:58 +0200

    add more steps

--- puppeteer/puppeteerFunctions.js
+++ puppeteer/puppeteerFunctions.js
@@ -23,20 +23,20 @@ export const getPrice = async (url, item) => {
 
     await page.goto('https://whatismyipaddress.com/');
     photos.push(await makeScreenshot(page));
-    // await page.goto(url);
+    await page.goto(url);
     // const photo1 = await page.screenshot({});
     // const photo1base64 = await photo1.toString('base64');
-    // photos.push(await makeScreenshot(page));
-    // await page.focus('#form-head-search-q');
-    // await page.keyboard.type(item);
-    // await page.keyboard.press('Tab');
-    // await page.keyboard.press('Enter');
+    photos.push(await makeScreenshot(page));
+    await page.focus('#form-head-search-q');
+    await page.keyboard.type(item);
+    await page.keyboard.press('Tab');
+    await page.keyboard.press('Enter');
 
-    // photos.push(await makeScreenshot(page));
-    // await page.waitForSelector('.js_seoUrl');
-    // await page.click('.js_seoUrl');
-    // await page.waitForTimeout(500);
-    // photos.push(await makeScreenshot(page));
+    photos.push(await makeScreenshot(page));
+    await page.waitForSelector('.js_seoUrl');
+    await page.click('.js_seoUrl');
+    await page.waitForTimeout(500);
+    photos.push(await makeScreenshot(page));
     // const photo2 = await page.screenshot({});
     // photo2base64 = await photo2.toString('base64');
     await browser.close();
